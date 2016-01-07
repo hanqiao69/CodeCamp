c3c18718c52dad6d9affd5ed490eb25393b3cdaa
Kelsey Hrubes <kelseyhrubes@gmail.com>
2016-01-06 21:18:11 -0800

    add profile nav stuff

---
+++ js/init.js
@@ -0,0 +1,13 @@
+
+// var NUM_NAV_ITEMS = 3;
+
+// function setNavigationBlocksHeight() {
+// 	var navItemHeight = ($("body").height())/NUM_NAV_ITEMS;
+
+// 	$( ".side-nav-item" ).each(function() {
+// 		console.log(navItemHeight);
+//   		 $( this ).height(navItemHeight);
+// 	});
+// }
+
+// setNavigationBlocksHeight();
